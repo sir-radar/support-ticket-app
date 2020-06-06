dc53707265330137dd45af3c50919e23bca29569
Samson Nwokike <samson.ikechukwu@softescu.com>
2020-06-06 14:21:16 +0100

    added tailwind css

--- .history/resources/views/welcome.blade_20200606123117.php
+++ .history/resources/views/welcome.blade_20200606123117.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Livewire App</title>
-        <link rel="stylesheet" href="/css/app.css">
+
         @livewireStyles
     </head>
     <body>
